93521fd368ab3cd42e6b7b1a1570f87e8356adeb
Jonathan <128359388+Teronega@users.noreply.github.com>
2023-09-01 02:47:10 -0400

    Add files via upload
    Tested functions via console.
    Added weather lines.

--- script.js
+++ script.js
@@ -3,26 +3,30 @@
 // 2. Create a function to display the cities
 // 3. Create a function to display the current weather
 // 4. Create a function to display the 5 day forecast
-// 5. Create a function to display the UV index (optional)
-// 6. Create a function to display the weather icons (optional)
 
 
-// Verify if function works.
+
+function addEventListeners() {
+    $("#search-button").on("click", function() {
+        var city = $("#city-input").val();
+        console.log(city);
+        displayCurrentWeather();
+    });
+}
+
+
+// Viewable on console; $.ajax calls to OpenWeather API
+// Once pulled, "then" shows the data in the console
 
 function displayCurrentWeather() {
     $.ajax({
-        url: current_weather_url,
-        method: "GET"
+        url: "https://api.openweathermap.org/data/2.5/weather?q=melbourne&appid=986630840632a725369847b0bec6ba74",
     }).then(function(response) {
         console.log(response);
-        var currentWeather = $("#current-weather");
-        currentWeather.empty();
-        var currentCity = $("<h3>").text(response.name);
-        var currentTemp = $("<p>").text("Temperature: " + response.main.temp);
-        var currentHumidity = $("<p>").text("Humidity: " + response.main.humidity);
-        var currentWind = $("<p>").text("Wind Speed: " + response.wind.speed);
-        var currentUV = $("<p>").text("UV Index: " + response.main.uv);
-        currentWeather.append(currentCity, currentTemp, currentHumidity, currentWind, currentUV);
+        console.log("Temperature", response.main.temp);
+        console.log("Feels Like", response.main.feels_like);
+        console.log("Wind Speed", response.wind.speed);
+        console.log("Humidity", response.main.humidity);
     });
 }
 
@@ -30,9 +34,9 @@ function displayCurrentWeather() {
 
 // OpenWeather API & Key////////////////////////////////
 
-url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
-currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
-APIkey = "986630840632a725369847b0bec6ba74"; // Generated API Key
+url = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";    
+currenturl = "api.openweathermap.org/data/2.5/forecast?q=";
+APIkey = "986630840632a725369847b0bec6ba74"; // Generated OpenWeather API Key
 queryurl = url + city + APIkey;
 current_weather_url = currenturl + city + APIkey; 
 
